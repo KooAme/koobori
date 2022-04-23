1918efc37b3e86b071927cc3e145b2b3aed2470a
Koobori <dg.changeworld@gmail.com>
2022-04-23 11:56:19 +0900

    Server connect
    Middleware review

--- src/server.js
+++ src/server.js
@@ -4,6 +4,27 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === '/protected') {
+    return res.send('Not');
+  }
+  console.log('allow');
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send('i want study hard');
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get('/', handleHome);
+
 const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT}⭐️`);
 };

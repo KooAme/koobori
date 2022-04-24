cbca3f939b7c9fe55141fe12950b034560a1b24f
Koobori <dg.changeworld@gmail.com>
2022-04-25 01:39:00 +0900

    morgan, Router
    i morgan and route set

---
+++ README.md
@@ -0,0 +1,17 @@
+# Reloaded plane
+
+Router
+
+/ > Home
+/join > Join
+/login > Login
+/search > Search
+
+/users/edit > Edit User
+/users/delete > Delete User
+
+/videos/watch > Watch Video
+/videos/edit > Edit Video
+/videos/delete > Delete Video
+/videos/comments > Comment on a video
+/videos/comments/delete > Delete a Comment on a video
--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === '/protected') {
-    return res.send('Not');
-  }
-  console.log('allow');
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send('i want study hard');
-};
-
+const logger = morgan('dev'); //have the other options
 app.use(logger);
-app.use(privateMiddleware);
-app.get('/', handleHome);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send('Home');
+
+const userRouter = express.Router();
+const handleUser = (req, res) => res.send('users');
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send('Watch videos');
+
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
+app.use('/', globalRouter);
 
 const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT}⭐️`);

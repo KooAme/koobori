bfa0f7bbad03491b67392eaa75a64cf27d96c20d
Koobori <dg.changeworld@gmail.com>
2022-05-24 01:11:03 +0900

    prac
    db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
+let videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 10,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 3,
+    comments: 6,
+    createdAt: '2 minutes ago',
+    views: 54,
+    id: 2,
+  },
+  {
+    title: 'Third Video',
+    rating: 4,
+    comments: 6,
+    createdAt: '2 minutes ago',
+    views: 54,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return res.render('home', { pageTitle: 'Home', videos });
 }; //(name, var);
 export const see = (req, res) => res.render('watch'); //console.log(req.params);

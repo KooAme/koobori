12bffbb2b06b1e352dbfc00d8eedde32529b36a6
Koobori <dg.changeworld@gmail.com>
2022-07-20 02:06:13 +0900

    templete pug
    conditionals, each, Iteration, Mixins ...

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,38 @@
-export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const see = (req, res) => res.render('watch');
+let videos = [
+  {
+    title: 'first video',
+    rating: 3,
+    comments: 2,
+    createAt: '2minutes ago',
+    views: 53,
+    id: 1,
+  },
+  {
+    title: 'second video',
+    rating: 5,
+    comments: 2,
+    createAt: '12minutes ago',
+    views: 33,
+    id: 2,
+  },
+  {
+    title: 'third video',
+    rating: 2,
+    comments: 2,
+    createAt: '4minutes ago',
+    views: 13,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  return res.render('home', { pageTitle: 'Home', videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render('edit');
 export const search = (req, res) => res.send('Search');
 export const upload = (req, res) => res.send('Upload');
-export const deleteVideo = (req, res) => {
-  return res.send('Delete Video');
-};
+export const deleteVideo = (req, res) => res.send('Delete Video');
